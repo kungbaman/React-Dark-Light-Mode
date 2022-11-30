95a023b03eca026656b83131a76a22c4aa57fa6e
kungbaman <natchapol.kamo@hotmail.com>
2022-11-30 07:55:42 +0700

    add Navbar and Content

---
+++ src/components/Content.js
@@ -0,0 +1,20 @@
+import dark from '../images/dark.svg'
+
+export const Content = () => {
+  return (
+    <main>
+      <div>
+        <h1>
+          Natchapol K <br />
+          Official
+        </h1>
+        <h3>DarkMode Workshop</h3>
+      </div>
+      <div>
+        <img src={dark} alt="Logo"/>
+      </div>
+    </main>
+  );
+};
+
+export default Content;
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav>
+        <div>
+            Mode [Light]
+        </div>
+        <div>
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
